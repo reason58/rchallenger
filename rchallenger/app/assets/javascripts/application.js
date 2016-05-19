1c28e1e77313d189230eefb9973b84c723d10ddb
Will Geiger <williamjgeiger@gmail.com>
2016-05-19 12:26:18 -0700

    Added log in dropdown and styling (temporary)

--- rchallenger/app/assets/javascripts/application.js
+++ rchallenger/app/assets/javascripts/application.js
@@ -14,3 +14,30 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function showLoginForm(){
+    console.log("Showing Login Form");
+    document.getElementById("logindropdown").classList.toggle("show");
+    console.log(document.getElementById('logindropdown'));
+
+}
+
+/* Possible pulldown functionality
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var d = 0; d < dropdowns.length; d++) {
+            var openDropdown = dropdowns [d];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+    */
